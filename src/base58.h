96a29aebad690907491abb6fa8f76775772dc0c1
mnull <n17r0u6@gmail.com>
2014-04-29 13:34:14 -0400

    addchg

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // taccoin addresses start with L
+        PUBKEY_ADDRESS = 26, // taccoin addresses start with T
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
